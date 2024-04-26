59a501437da7854b73f32bb5a7d78888543a6eec
Photon2002 <132060233+Photon2002@users.noreply.github.com>
2024-04-26 20:43:03 +0200

    test

--- Pierogi Entropii/Pierogi Entropii.c
+++ Pierogi Entropii/Pierogi Entropii.c
@@ -16,7 +16,7 @@ int example2(void) {
 #endif
 
     while (!WindowShouldClose()) {
-        // lol2
+        // lol3
     }
 
     CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
