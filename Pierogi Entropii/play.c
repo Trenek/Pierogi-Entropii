29d2e7d0cbabf64b0a5a1907eddc5767f7d97b27
Trenek <mateusz.sas@vp.pl>
2024-04-26 22:18:24 +0200

    update

--- Pierogi Entropii/play.c
+++ Pierogi Entropii/play.c
@@ -6,10 +6,25 @@
 
 #include "drawMenuElement.h"
 
+struct GridTile {
+    Vector2 coordinates;
+    Color color;
+    Texture2D *texture;
+    Texture2D *object;
+};
+
+struct GridTile **allocGridTile(int width, int height, Texture2D *texture, const char *file) {
+
+}
+
 void play(enum state *state) {
     Color color = { .r = 100, .g = 100, .b = 100, .a = 255 };
     Color color2 = { .r = 78, .g = 215, .b = 50, .a = 255 };
     Color color3 = { .r = 78, .g = 215, .b = 50, .a = 105 };
+    int width = 8;
+    int height = 10;
+
+
 
     while (!WindowShouldClose() && *state == PLAY) {
         BeginDrawing();
