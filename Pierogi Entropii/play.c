6e36e3ccfe324b33a1c6f3334d15e2670a2bde9d
Trenek <mateusz.sas@vp.pl>
2024-04-27 16:31:45 +0200

    Naprawa

--- Pierogi Entropii/play.c
+++ Pierogi Entropii/play.c
@@ -199,7 +199,7 @@ void play(enum state *state) {
                             else {
                                 player.coordinates = map[num].position;
                             }
-                            player.coordinates = grid[interY][interX].expectedCoordinates;
+                           // player.coordinates = grid[interY][interX].expectedCoordinates;
                             interact = false;
                         }
                         break;
